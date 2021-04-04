429e6bead6429695f4f9dfc48b463360adaf151b
Henrique Hoinacki <riqueah@gmail.com>
2021-04-04 11:40:56 -0300

    new deploy

---
+++ precache-manifest.d83540740b7f6cab296c9590023c6417.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fcb20c5ef3b050c6a0",
+    "url": "/css/about.a1c3c0aa.css"
+  },
+  {
+    "revision": "ae9ad27bafc5d35fc556",
+    "url": "/css/app.6127bf8f.css"
+  },
+  {
+    "revision": "d32c1abcb2db1bcdf9ba",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "9552428204cc465b9c379eefa3727a66",
+    "url": "/img/avatar.95524282.png"
+  },
+  {
+    "revision": "27782f1d23abcf4f5edcc08d55ce4315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59fcb20c5ef3b050c6a0",
+    "url": "/js/about.a8aa590f.js"
+  },
+  {
+    "revision": "ae9ad27bafc5d35fc556",
+    "url": "/js/app.d62aa520.js"
+  },
+  {
+    "revision": "d32c1abcb2db1bcdf9ba",
+    "url": "/js/chunk-vendors.4fc35eca.js"
+  },
+  {
+    "revision": "e4bfb27f90be208d1e574f5dedfd195f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
